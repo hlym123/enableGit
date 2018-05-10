2c5ed63fd40f363b3152e213fd628b694155c320
hlym123 <lwylwt@qq.com>
2018-05-10 09:21:58 +0800

    add OpenMV demo

--- OpenMV use note.py
+++ OpenMV use note.py
@@ -6,3 +6,19 @@
 
 '''
 
+
+# Quick reference for the openmvcam
+
+'''
+ 1.1 General board control 
+'''
+import pyb
+
+pyb.repl_uart(pyb.UART(3, 9600, timeout_char=1000)) # duplicate REPL on UART(3)
+pyb.wfi() # pause CPU, waiting for interrupt
+pyb.stop() # stop CPU, waiting for external interrupt
+
+
+'''
+ 1.2 Delay and timing
+'''
