471933bb5c3fe9984e7f74c6f11476a28dd3310d
Create123 <lwylwt@qq.com>
2018-06-14 22:37:22 +0800

    daily

--- en.cpp
+++ en.cpp
@@ -3,6 +3,69 @@
 	@date:2018/6/6
 
 *************************************************************************************/
+ 
+
+1. @2018/6/11
+unbearable
+violate
+orbit
+
+2. @2018/6/12	
+communism
+get through
+get on
+come down
+cousin
+firewood
+gather
+get toghter
+cough
+conscience
+evaluate
+energy
+chimney
+pyramid
+forest
+comma
+hen
+miniskirt
+
+3. @2018/6/13
+cosy
+yoghurt
+tournament
+skyscraper
+picnic
+toilet
+irrigation
+palace
+prison
+privilege
+thief
+shaver
+Swiss
+messy
+petrol
+gruel
+literature
+in other words
+complusory
+drawn
+geese
+
+4. @2018/6/14
+bureaucratic
+laborer
+
+
+
+
+
+
+
+ 
+
+
 
 // ===================================================================================
 // Level 1
@@ -29,7 +92,7 @@ The company may have to alter their policy on algitural imports.
 1. alter
 2. agriculture
 
-The driver wasb leeding heavily when the ambulance arrived at the accident site.
+The driver was bleeding heavily when the ambulance arrived at the accident site.
 //
 3. bleed
 4. heavily
