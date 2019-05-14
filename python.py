309184769513633d4ce2cd56cc11d7a62dece14f
hlym123 <lwylwt@qq.com>
2019-05-14 09:41:27 +0800

    test

--- python.py
+++ python.py
@@ -179,7 +179,18 @@ class Student(object):
 	
 # 面向对象高级编程 ==================================================================================
 
+# demo:
+class Student(object):
 
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+		
+		
+		
 
 # 错误、调试和测试 ==================================================================================
 
